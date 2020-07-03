8336212e0a99b011dda657ce295652e941a252ca
Marvin Hülsmann <mrntv64@gmail.com>
2020-07-03 20:33:16 +0200

    removed invalid functions for labymod

---
+++ src/main/java/de/marvhuelsmann/labymodaddon/LabyHelpAddon.java
@@ -0,0 +1,100 @@
+package de.marvhuelsmann.labymodaddon;
+
+import de.marvhuelsmann.labymodaddon.menu.*;
+import de.marvhuelsmann.labymodaddon.module.TexturePackModule;
+import de.marvhuelsmann.labymodaddon.util.Commands;
+import net.labymod.api.events.MessageSendEvent;
+import net.labymod.main.LabyMod;
+import net.labymod.settings.elements.BooleanElement;
+import net.labymod.settings.elements.ControlElement;
+import net.labymod.settings.elements.SettingsElement;
+import net.labymod.utils.Consumer;
+import net.labymod.utils.Material;
+import net.labymod.utils.ServerData;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumChatFormatting;
+
+import java.util.List;
+import java.util.UUID;
+
+
+public class LabyHelpAddon extends net.labymod.api.LabyModAddon {
+
+    public static boolean AddonEnable = true;
+    private boolean AddonHelpMessage = true;
+
+
+    @Override
+    public void onEnable() {
+        this.getApi().registerForgeListener(false);
+
+        this.getApi().registerModule(new TexturePackModule());
+
+
+        LabyMod.getInstance().getLabyModAPI().updateCurrentGamemode("LabyHelp");
+
+
+        LabyMod.getInstance().getChatToolManager().getPlayerMenu().add(new BandanaMenu());
+        LabyMod.getInstance().getChatToolManager().getPlayerMenu().add(new CapeMenu());
+        LabyMod.getInstance().getChatToolManager().getPlayerMenu().add(new SkinMenu());
+        LabyMod.getInstance().getChatToolManager().getPlayerMenu().add(new CosmeticsClearerMenu());
+
+
+        LabyMod.getInstance().getChatToolManager().getPlayerMenu().add(new DanceMenu());
+
+
+        this.getApi().getEventManager().registerOnJoin(new Consumer<ServerData>() {
+            @Override
+            public void accept(ServerData serverData) {
+                if (AddonHelpMessage) {
+                    LabyMod.getInstance().notifyMessageRaw("LabyAddon | Help", "Benutze /LhHelp um alle Befehle zu sehen!");
+                }
+            }
+        });
+
+
+        this.getApi().getEventManager().register(new MessageSendEvent() {
+            @Override
+            public boolean onSend(String message) {
+                if (message.startsWith("/bandana") || message.startsWith("/cape") || message.startsWith("/skin") || message.startsWith("cosmeticsCC") || message.startsWith("/LhHelp")) {
+                    Commands.CommandMessage(message);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void loadConfig() {
+        AddonEnable = !this.getConfig().has("enable") || this.getConfig().get("enable").getAsBoolean();
+        this.AddonHelpMessage = !this.getConfig().has("enable") || this.getConfig().get("enable").getAsBoolean();
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+        final BooleanElement booleanElement = new BooleanElement("Addon Aktiviert", new ControlElement.IconData(Material.GOLD_INGOT), new Consumer<Boolean>() {
+            @Override
+            public void accept(final Boolean enable) {
+                AddonEnable = enable;
+
+                LabyHelpAddon.this.getConfig().addProperty("enable", enable);
+                LabyHelpAddon.this.saveConfig();
+            }
+        }, AddonEnable);
+
+        final BooleanElement booleanElement2 = new BooleanElement("Hilfe beitritts Nachricht", new ControlElement.IconData(Material.REDSTONE_COMPARATOR), new Consumer<Boolean>() {
+            @Override
+            public void accept(final Boolean enable) {
+                LabyHelpAddon.this.AddonHelpMessage = enable;
+
+                LabyHelpAddon.this.getConfig().addProperty("enable", enable);
+                LabyHelpAddon.this.saveConfig();
+            }
+        }, this.AddonHelpMessage);
+
+
+        list.add(booleanElement);
+        list.add(booleanElement2);
+    }
+}
---
+++ src/main/java/de/marvhuelsmann/labymodaddon/util/Commands.java
@@ -0,0 +1,48 @@
+package de.marvhuelsmann.labymodaddon.util;
+
+import de.marvhuelsmann.labymodaddon.LabyHelpAddon;
+import de.marvhuelsmann.labymodaddon.LabyPlayer;
+import net.labymod.main.LabyMod;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumChatFormatting;
+
+import java.util.UUID;
+
+public class Commands {
+
+    private static UUID getUUID(String name) {
+        UUID uuid;
+        uuid = MinecraftServer.getServer().getPlayerProfileCache().getGameProfileForUsername(name).getId();
+        return uuid;
+    }
+
+    public static void CommandMessage(String message) {
+        final LabyPlayer labyPlayer = new LabyPlayer();
+        if (LabyHelpAddon.AddonEnable) {
+            if (message.startsWith("/bandana")) {
+                //SPACE MUST BE THERE
+                UUID uuid = getUUID(message.replaceAll("/bandana ", ""));
+                labyPlayer.openBandanaUrl(uuid);
+            } else if (message.startsWith("/cape")) {
+                //SPACE MUST BE THERE
+                UUID uuid = getUUID(message.replaceAll("/cape ", ""));
+                labyPlayer.openCapeUrl(uuid);
+            } else if (message.startsWith("/skin")) {
+                //SPACE MUST BE THERE
+                UUID uuid = getUUID(message.replaceAll("/skin ", ""));
+                labyPlayer.openSkin(uuid);
+            }  else if (message.startsWith("/cosmeticsCC")) {
+                UUID uuid = getUUID(message.replaceAll("/cosmeticsCC ", ""));
+                LabyMod.getInstance().getUserManager().getUser(uuid).getCosmetics().clear();
+            }
+            if (message.equalsIgnoreCase("/LhHelp")) {
+                labyPlayer.sendMessage("- /bandana <Spieler>");
+                labyPlayer.sendMessage("- /cape <Spieler>");
+                labyPlayer.sendMessage("- /skin <Spieler>");
+                labyPlayer.sendMessage("- /cosmeticsCC <Spieler>");
+            }
+        } else {
+            labyPlayer.sendMessage(EnumChatFormatting.RED + "Du hast das LabyAddon deaktiviert!");
+        }
+    }
+}
